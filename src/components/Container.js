1bb5990366f7f5f80d1cece3bfa6ca835dc4cf1a
Chaw Chit Su Thwe <scm.chawchitsuthwe@gmail.com>
2022-02-02 17:30:12 +0630

    finished remove feature, filters

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,16 +1,69 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoItem from "./TodoItem";
 
 const Container = () => {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  const [clear, setClear] = useState(false);
+  const [type, setType] = useState("all");
+
+  useEffect(() => {
+    if (type === "all") {
+      setFilteredTodos(todos);
+    } else if (type === "active") {
+      setFilteredTodos(todos.filter(todo => todo.checked === false));
+    } else if (type === "completed") {
+      setFilteredTodos(todos.filter(todo => todo.checked === true));
+    }
+  }, [type, todos]);
 
   const handleEnter = event => {
     if (event.keyCode === 13) {
-      console.log(event.target.value);
-      setTodos(todos => [...todos, event.target.value]);
+      setTodos(todos => [...todos, {
+        text: event.target.value,
+        checked: false
+      }]);
       setText("");
     }
+  }
+
+  const handleDelete = (id) => {
+    var todosArray = [...todos];
+    if (id !== -1) {
+      todosArray.splice(id, 1);
+      setTodos(todosArray);
+    }
+  }
+
+  const handleCheck = (id, checkedParam) => {
+    var todosArray = [...todos];
+    var todo = {...todosArray[id]}
+    if (checkedParam) {
+      todo.checked = true;
+      setClear(false);
+    } else {
+      todo.checked = false;
+    }
+    todosArray[id] = todo;
+    setTodos(todosArray);
+  }
+
+  const handleClear = () => {
+    setTodos(todos => todos.filter(todo => todo.checked === false));
+    setClear(true);
+  }
+
+  const handleAll = () => {
+    setType("all");
+  }
+
+  const handleActive = () => {
+    setType("active");
+  }
+
+  const handleCompleted = () => {
+    setType("completed");
   }
 
   return (
@@ -21,10 +74,25 @@ const Container = () => {
       </div>
       <div className="todo-list">
       {
-        todos.map((todo, index) => 
-          <TodoItem key={index} todo={todo} />
+        filteredTodos.map((todo, index) => 
+          <TodoItem 
+            key={index}
+            id={index}
+            todo={todo}
+            clear={clear}
+            handleDelete={handleDelete}
+            handleCheck={handleCheck} />
         )
       }
+        <div className="todo-item-wrapper foot-nav">
+          <p className="left-item">{todos.filter(todo => todo.checked === false).length} items left</p>
+          <div className="action-btns">
+            <button className={(type==="all" ? "active" : "")} onClick={handleAll}>All</button>
+            <button className={(type==="active" ? "active" : "")} onClick={handleActive}>Active</button>
+            <button className={(type==="completed" ? "active" : "")} onClick={handleCompleted}>Completed</button>
+          </div>
+          <button onClick={handleClear} className="clear-btn">Clear Completed</button>
+        </div>
       </div>
     </div>
   )
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const TodoItem = props => {
+  const [strike, setStrike] = useState("todo-text");
+
+  useEffect(() => {
+    if(props.clear) {
+      setStrike("todo-text");
+    }
+    if (props.todo.checked) {
+      setStrike("todo-text strikethrough");
+    } else {
+      setStrike("todo-text");
+    }
+  }, [props.clear, props.todo]);
+
+  const handleCheck = () => {
+    if (strike === "todo-text") {
+      setStrike("todo-text strikethrough");
+      props.handleCheck(props.id, true);
+    } else {
+      setStrike("todo-text");
+      props.handleCheck(props.id, false);
+    }
+  }
+
   return (
     <div className="todo-item-wrapper">
-      <p className="todo-text">{props.todo}</p>
+      <input type="checkbox" onChange={handleCheck} checked={props.todo.checked } />
+      <p className={strike}>{props.todo.text}</p>
+      <button onClick={() => props.handleDelete(props.id)}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+      </button>
     </div>
   )
 }
