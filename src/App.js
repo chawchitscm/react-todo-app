ac2669d7e00ebc87d760db2c60b9346e28df540f
Chaw Chit Su Thwe <scm.chawchitsuthwe@gmail.com>
2022-02-02 20:17:15 +0630

    finished light/dark feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
-import Container from './components/Container';
+import "./App.css";
+import Container from "./components/Container";
+import useLocalStorage from "use-local-storage";
 
 function App() {
+  const defaultLight = window.matchMedia("(prefers-color-scheme: light)").matches;
+  const [theme, setTheme] = useLocalStorage("theme", defaultLight ? "light" : "dark");
+
+  const switchTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+  }
   return (
-    <div className="App">
-      <Container />
+    <div className="App" data-theme={theme}>
+      <Container 
+        theme={theme}
+        switchTheme={switchTheme} 
+      />
     </div>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -26,7 +26,10 @@ const TodoItem = props => {
 
   return (
     <div className="todo-item-wrapper">
-      <input type="checkbox" onChange={handleCheck} checked={props.todo.checked } />
+      <div className="round">
+        <input type="checkbox" id={props.id} onChange={handleCheck} checked={props.todo.checked} />
+        <label htmlFor={props.id}></label>
+      </div>
       <p className={strike}>{props.todo.text}</p>
       <button onClick={() => props.handleDelete(props.id)}>
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
